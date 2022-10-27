f7855018b69d2b2e0d8e5e2fd24c53b12ad2fb62
Drew <101952850+c-drew@users.noreply.github.com>
2022-10-27 13:04:30 -0400

    Update nestedtriangle.java
    watched a youtube video...

--- Assignment 4/src/nestedtriangle.java
+++ Assignment 4/src/nestedtriangle.java
@@ -5,8 +5,15 @@ public class nestedtriangle {
 		
 		System.out.println("This program prints a 10x10 right triangle.\n");
 		
-		for (int i = 0; i <= 10; i++) {
-			System.out.println("*");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j < i) {
+				System.out.print(" ");
+				}
+				else 
+				System.out.print("*");
+		}
+		System.out.println();
 		}
 		
 	}
